87d6658a695d113e632ad00e971358ea5b1e0d67
Hui-Sin <20018354@myrp.edu.sg>
2021-05-21 14:22:07 +0800

    Part 1 done edited

--- app/src/main/java/sg/edu/rp/c346/id20018354/demoriddle/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20018354/demoriddle/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,9 +15,10 @@ public class MainActivity extends AppCompatActivity {
     TextView tvQ2;
     Button btnrevealQ2;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "onCreate() called.");
         tvQ1=findViewById(R.id.textViewQ1);
         btnrevealQ1=findViewById(R.id.buttonRevealQ1);
         tvQ2=findViewById(R.id.textViewQ2);
@@ -38,4 +40,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    protected void onStart() {
+        Log.d("MainActivity", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("MainActivity", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("MainActivity", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("MainActivity", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("MainActivity", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("MainActivity", "onRestart() called.");
+        super.onRestart();
+    }
+
 }
